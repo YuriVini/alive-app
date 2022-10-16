41b80a978f945530ec31f0d8f99ed445ccb1aac1
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2022-10-16 12:18:15 -0300

    feat: colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+export default {
+  // Basic colors
+  white: "#FFFFFF",
+  black: "#000000",
+
+  // Blue colors
+  blueHeader: "#4044C8",
+
+  // Gray colors
+  text: "#686868",
+  textLight: "#989898",
+  grayNeutral: "#F2F2F2",
+  grayFooter: "#F6F6F6",
+
+  //LinearGradient
+  linearCard: ["#155efb", "#00cdfd"],
+
+  overlay: "rgba(0,0,0,0.3)",
+
+  // Transparent color
+  none: "transparent",
+};
